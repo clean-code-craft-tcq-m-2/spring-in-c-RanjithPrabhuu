b290f52b5cdeaeb79499258c7956cea63b3bfee9
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-10-14 06:55:09 +0530

    Update stats.c
    Initial commit

--- stats.c
+++ stats.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
@@ -5,7 +6,38 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    int counter = 0;
+    float sum = 0, min, max;
+    if((numberset != (void *)0) || (setlength > 0))
+    {
+        min = numberset[0];
+        max = numberset[0];
+        
+        while(counter < setlength)
+        {
+            sum += numberset[counter];
+            if(min > numberset[counter])
+            {
+                min = numberset[counter];
+            }
+            if(max < numberset[counter])
+            {
+                max = numberset[counter];
+            }
+            
+            counter++;
+        }
+        s.average = sum / setlength;
+        s.min = min;
+        s.max = max;
+    }
+    else
+    {
+        s.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
+    }
+    
+    return s;
 }
-
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
