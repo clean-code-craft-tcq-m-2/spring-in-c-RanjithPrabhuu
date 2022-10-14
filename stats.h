61725d58d77cbc0db6a359dde2bc9c44d25c8957
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-10-14 15:25:03 +0530

    Update stats.h
    cleared spaces

--- stats.h
+++ stats.h
@@ -11,9 +11,3 @@ struct Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], int alerterCount, struct Stats computedStats);
 
-void emailAlerter(void);
-void ledAlerter(void);
-
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
-
