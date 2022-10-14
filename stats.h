573de715f316d4197f1c4ecd59fe8d2720a819a3
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2022-10-14 07:03:04 +0530

    Update stats.h
    updated the extern variables and struct declaration

--- stats.h
+++ stats.h
@@ -1,8 +1,19 @@
 
+struct Stats{
+  float min;
+  float max;
+  float average;
+};
+
+
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], int alerterCount, struct Stats computedStats);
 
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
+void emailAlert(void);
+void ledAlert(void);
+
+extern int emailAlertCount;
+extern int ledAlertCount;
+
